54813f667c7de073e302e3d5668ae91b428e9c89
Kr0n0 <ccvals@linux.com>
2017-03-21 10:44:02 +0100

    Modificado Android.mk para biblioteca / ejecutable y compilacion PIE (Android >= 5.x)

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,8 +4,16 @@ FICHEROS_FUENTE	:=	hello_world.c
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS) 
+
+
+# Habilitar PIE (Position Independent Executables) para Android >= 5.x
+LOCAL_CFLAGS += -fPIE
+LOCAL_LDFLAGS += -fPIE -pie
+
 LOCAL_MODULE    := $(NOMBRE_PROYECTO) 
 LOCAL_SRC_FILES := $(FICHEROS_FUENTE)
 
-# Comentar si se quiere generar una biblioteca en vez de un ejecutable
-include $(BUILD_EXECUTABLE)
+# Crear ejecutable (ELF)
+#include $(BUILD_EXECUTABLE)
+# Crear biblioteca (.so)
+include $(BUILD_SHARED_LIBRARY)
